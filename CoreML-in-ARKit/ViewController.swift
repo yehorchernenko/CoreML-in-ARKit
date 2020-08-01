2d00fc9c435f83491353f6f1fbe2a25de040e0c1
Yehor Chernenko <yehor_chernenko@epam.com>
2020-08-01 12:08:40 +0300

    Initial commit

---
+++ CoreML-in-ARKit/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  CoreML-in-ARKit
+//
+//  Created by Yehor Chernenko on 01.08.2020.
+//  Copyright © 2020 Yehor Chernenko. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sceneView.delegate = self
+        sceneView.scene = SCNScene()
+        
+        // Debug
+        sceneView.showsStatistics = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        startSession()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        stopSession()
+    }
+    
+    private func startSession() {
+        guard ARWorldTrackingConfiguration.isSupported else {
+            assertionFailure("ARKit is not supported")
+            return
+        }
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        
+        sceneView.session.run(configuration)
+    }
+    
+    private func stopSession() {
+        sceneView.session.pause()
+    }
+}
